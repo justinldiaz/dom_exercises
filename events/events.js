6a4b15c797674850b82df13766ba88acdb0be6d8
Justin <justindiaz91@gmail.com>
2015-05-01 00:33:35 -0700

    My homework

--- events/events.js
+++ events/events.js
@@ -4,4 +4,13 @@ var but = document.querySelector("#clicker");
 but.addEventListener("click", function( event ){
     alert("Hello World")
 });
+
+var box = document.querySelector("#mouser");
+box.onmouseover = function(event){
+	box.style.backgroundColor = "yellow";
+}
+box.onmouseout = function(event){
+	box.style.backgroundColor = "green";
+}
+
 });
