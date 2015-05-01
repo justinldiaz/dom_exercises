a342ac342c290cc6f4c92687404d08ca951775b5
Justin <justindiaz91@gmail.com>
2015-05-01 00:06:45 -0700

    My homework

--- events/events.js
+++ events/events.js
@@ -1,1 +1,12 @@
-// Your code here
+window.addEventListener("load", function (){
+
+var but = document.querySelector("#clicker");
+but.addEventListener("click", function( event ){
+    alert("Hello World")
+});
+var box = document.querySelector("mouser");
+box.addEventListener("mouseover", function( event ){
+ .style.color= "yellow";
+})
+//addEventListener("submit", function())
+});
